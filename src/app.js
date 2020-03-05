a54906f50fac3505816a58bc0ff865e7b4675154
Max Kharandziuk <max.kharandziuk@gmail.com>
2020-03-05 15:54:21 +0100

    removed hooks

--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
-import React, { useEffect, useState, Suspense } from 'react'
+import React, { useState, Suspense } from 'react'
 
 import { BrowserRouter } from 'react-router-dom'
 
-import useTheme from './hooks/theme.hook'
 import Routers from './routes'
 import { useStoreState } from './store'
 
 const Loader = () => <>Loader</>
 
 const App = () => {
-  const { theme } = useTheme()
-
   const [isAuthenticated, setIsAuthenticated] = useState(false)
 
   return (
--- src/hooks/theme.hook.js
+++
@@ -1,9 +0,0 @@
-import Colors from '../common/colors'
-import { Fonts, FontSizes } from '../common/fonts'
-import { pxToRem } from '../utils/styles'
-
-function useCustomTheme() {
-  return {}
-}
-
-export default useCustomTheme
